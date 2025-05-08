c0cd3705049875bf56636c25ff467fb8524cd3b1
tharamae <sonluna424@gmail.com>
2025-05-08 17:29:37 +0800

    third commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +7,6 @@ app = Flask(__name__)
 def home():
     return 'Thara Mae C. Tuñacao <br> BSIT-3 1st 25 <br> System Integration and Architecture 1 <br> Semi-Final Exam'
 
+if __name__=='__main__':
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0', port=port)
